4df5e3eb8f1f4be9c8e07d6da7e19b37f33c40df
Torimune29 <71534350+Torimune29@users.noreply.github.com>
2023-01-22 16:37:45 +0900

    refactor: flake

--- flake.nix
+++ flake.nix
@@ -15,110 +15,11 @@
       pkgs = import nixpkgs {inherit system;};
       pre-commit = import ./tools/pre-commit {inherit system pre-commit-hooks;};
       defaultCommands = import ./tools/commands {inherit pkgs;};
-      commandHelperSource = {
-        pkgs,
-        commands,
-        shell ? "mkShellNoCC",
-        shellHook ? "",
-        nativeBuildInputs ? [],
-        ...
-      }: let
-        descriptionDefaultText = "(No description.)";
-        exampleDefaultText = "(No example.)";
-        # generate help
-        usage = pkgs.lib.concatStrings (
-          pkgs.lib.forEach commands (command: ''
-            ${builtins.toString command.name}
-                description:
-                    ${
-              if command.description == ""
-              then descriptionDefaultText
-              else builtins.toString command.description
-            }
-                example:
-                    ''$ ${
-              if command.example == ""
-              then exampleDefaultText
-              else builtins.toString command.example
-            }
-          '')
-        );
-        help = pkgs.writeShellScriptBin "help-commands" ''
-          set -euo pipefail
-          cat <<EOF
-          ${usage}
-          EOF
-        '';
-
-        # for check command examples validation
-        examples = pkgs.lib.concatStrings (
-          pkgs.lib.forEach commands (command:
-            pkgs.lib.optionalString
-            (command.example != exampleDefaultText)
-            (builtins.toString command.example + "\n"))
-        );
-        testExamples = pkgs.writeShellScriptBin "test-command-examples" ''
-          set -euo pipefail
-          ${examples}
-        '';
-
-        # generate usage markdown
-        usageMarkdown =
-          ''
-            <!-- markdownlint-disable -->
-            # Command usage
-
-          ''
-          + pkgs.lib.concatStrings (
-            pkgs.lib.forEach commands (
-              command: ''
-                ## ${builtins.toString command.name}
-
-                ### description
-
-                ${
-                  if command.description == ""
-                  then descriptionDefaultText
-                  else builtins.toString command.description
-                }
-
-                ### example
-
-                \`\`\`bash
-                ${
-                  if command.example == ""
-                  then exampleDefaultText
-                  else builtins.toString command.example
-                }
-                \`\`\`
-
-              ''
-            )
-          );
-        generateDocument = pkgs.writeShellScriptBin "generate-document" ''
-          set -euo pipefail
-          cat <<EOF
-          ${usageMarkdown}
-          EOF
-        '';
-
-        # for passing to devShells
-        derivations = pkgs.lib.forEach commands (command: command.package);
-      in
-        pkgs."${shell}" {
-          shellHook = shellHook;
-          nativeBuildInputs =
-            nativeBuildInputs
-            ++ derivations
-            ++ [help testExamples generateDocument]
-            # ++ [
-            #   clang
-            # ]
-            ;
-        };
+      # commandHelper = import ./tools/command-helper;
+      command-helper = import ./src/command-helper;
     in {
       packages = rec {
-        commandHelper = commandHelperSource;
+        commandHelper = command-helper;
         default = commandHelper;
       };
 
